7f782efa9d0ad62da5d5e95c5a446e869bb78611
Zion Ng <zion@intzone.com>
2018-10-21 15:29:53 +0800

    Output JSON for test

--- tests/test.php
+++ tests/test.php
@@ -1,14 +1,10 @@
 <?php
-$rootDir = __DIR__ . '/../';
+$dir = __DIR__ . '/../';
 require $dir . 'vendor/autoload.php';
 
 // SGQR code sample from SOBA EXPRESS in article https://www.channelnewsasia.com/news/singapore/sgqr-qr-code-cashless-payment-singapore-rolls-out-unified-10727568
 $qrCode = '00020101021126810011SG.COM.NETS01231198500065G9912312359000211111686614000308686614019908604108C251800007SG.SGQR01121809072DD85C020701.00010306079027040201050206060400000708201809155204581253037025802SG5912SOBA EXPRESS6009Singapore630457B3';
 
-$specs = include $rootDir . 'config/sgqr-specs.php';
-$parser = new \App\SgQrParser($specs);
+$parser = new \SgQr\Parser();
 $result = $parser->parse($qrCode);
-
-echo '<pre>';
-print_r($result);
-echo '</pre>';
+echo json_encode($result, JSON_PRETTY_PRINT);
